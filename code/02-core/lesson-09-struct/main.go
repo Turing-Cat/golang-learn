6699c1a16fb50f5f345b82c3bcc3374a27fe1c41
jhzhang <1837046433@qq.com>
2026-04-25 10:57:42 +0800

    Add 2.2 method exercise: Book with PrintInfo and Discount
    - PrintInfo: value receiver method
    - Discount: pointer receiver method to modify original price

--- code/02-core/lesson-09-struct/main.go
+++ code/02-core/lesson-09-struct/main.go
@@ -8,6 +8,13 @@ type Book struct {
 	Price  float64
 }
 
+func (b Book) PrintInfo() {
+	fmt.Printf("%s的作者是%s，价格是%.2f\n", b.Title, b.Author, b.Price)
+}
+
+func (b *Book) Discount(pct float64) {
+	b.Price = b.Price * pct
+}
 func main() {
 	b1 := Book{
 		Title:  "《Go语言实战》",
@@ -15,5 +22,7 @@ func main() {
 		Price:  99.89,
 	}
 
-	fmt.Printf("%s的作者是%s，价格是%.2f", b1.Title, b1.Author, b1.Price)
+	b1.PrintInfo()
+	b1.Discount(0.9)
+	b1.PrintInfo()
 }
